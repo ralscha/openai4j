e0ec962e088187a766a60d54662293dcd14c713a
Ralph Schaer <ralphschaer@gmail.com>
2023-11-19 16:35:16 +0100

    Hand crafted builders

--- src/main/java/ch/rasc/openai4j/Configuration.java
+++ src/main/java/ch/rasc/openai4j/Configuration.java
@@ -15,10 +15,8 @@
  */
 package ch.rasc.openai4j;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
-
-import org.immutables.value.Value;
-import org.immutables.value.Value.Style.ImplementationVisibility;
 
 import feign.Client;
 import feign.Logger;
@@ -30,75 +28,196 @@ import feign.codec.ErrorDecoder;
 import feign.http2client.Http2Client;
 import feign.slf4j.Slf4jLogger;
 
-@Value.Immutable
-@Value.Style(visibility = ImplementationVisibility.PACKAGE, depluralize = true)
-public interface Configuration {
+@SuppressWarnings({ "hiding", "unused" })
+public class Configuration {
 
-	static Builder builder() {
+	private final String apiKey;
+	private final String organization;
+	private final String baseUrl;
+	private final Client client;
+	private final Retryer retryer;
+	private final Request.Options feignOptions;
+	private final Logger logger;
+	private final ErrorDecoder errorDecoder;
+	private final RequestInterceptor additionalRequestInterceptor;
+	private final Level logLevel;
+	private final String azureEndpoint;
+	private final String apiVersion;
+	private final String azureDeployment;
+
+	private Configuration(Builder builder) {
+		if (builder.apiKey == null) {
+			throw new IllegalArgumentException("apiKey must not be null");
+		}
+
+		this.apiKey = builder.apiKey;
+		this.organization = builder.organization;
+
+		this.baseUrl = Objects.requireNonNullElse(builder.baseUrl,
+				"https://api.openai.com/v1");
+		this.client = Objects.requireNonNullElse(builder.client, new Http2Client());
+		this.retryer = Objects.requireNonNullElse(builder.retryer, new Retryer.Default(
+				TimeUnit.SECONDS.toMillis(2), TimeUnit.SECONDS.toMillis(2), 3));
+		this.feignOptions = Objects.requireNonNullElse(builder.feignOptions,
+				new Request.Options(10, TimeUnit.SECONDS, 60, TimeUnit.SECONDS, true));
+		this.logger = Objects.requireNonNullElse(builder.logger, new Slf4jLogger());
+		this.errorDecoder = Objects.requireNonNullElse(builder.errorDecoder,
+				new OpenAIErrorDecoder());
+		this.additionalRequestInterceptor = builder.additionalRequestInterceptor;
+		this.logLevel = Objects.requireNonNullElse(builder.logLevel, Level.NONE);
+		this.azureEndpoint = builder.azureEndpoint;
+		this.apiVersion = builder.apiVersion;
+		this.azureDeployment = builder.azureDeployment;
+	}
+
+	public static Builder builder() {
 		return new Builder();
 	}
 
-	String apiKey();
+	public static final class Builder {
+		private String apiKey;
+		private String organization;
+		private String baseUrl;
+		private Client client;
+		private Retryer retryer;
+		private Request.Options feignOptions;
+		private Logger logger;
+		private ErrorDecoder errorDecoder;
+		private RequestInterceptor additionalRequestInterceptor;
+		private Level logLevel;
+		private String azureEndpoint;
+		private String apiVersion;
+		private String azureDeployment;
 
-	@Nullable
-	String organization();
+		private Builder() {
+		}
 
-	@Value.Default
-	default String baseUrl() {
-		return "https://api.openai.com/v1";
+		public Builder apiKey(String apiKey) {
+			this.apiKey = apiKey;
+			return this;
+		}
+
+		public Builder organization(String organization) {
+			this.organization = organization;
+			return this;
+		}
+
+		public Builder baseUrl(String baseUrl) {
+			this.baseUrl = baseUrl;
+			return this;
+		}
+
+		public Builder client(Client client) {
+			this.client = client;
+			return this;
+		}
+
+		public Builder retryer(Retryer retryer) {
+			this.retryer = retryer;
+			return this;
+		}
+
+		public Builder feignOptions(Request.Options feignOptions) {
+			this.feignOptions = feignOptions;
+			return this;
+		}
+
+		public Builder logger(Logger logger) {
+			this.logger = logger;
+			return this;
+		}
+
+		public Builder errorDecoder(ErrorDecoder errorDecoder) {
+			this.errorDecoder = errorDecoder;
+			return this;
+		}
+
+		public Builder additionalRequestInterceptor(
+				RequestInterceptor additionalRequestInterceptor) {
+			this.additionalRequestInterceptor = additionalRequestInterceptor;
+			return this;
+		}
+
+		public Builder logLevel(Level logLevel) {
+			this.logLevel = logLevel;
+			return this;
+		}
+
+		/**
+		 * The name of your Azure OpenAI Resource.
+		 */
+		public Builder azureEndpoint(String azureEndpoint) {
+			this.azureEndpoint = azureEndpoint;
+			return this;
+		}
+
+		public Builder apiVersion(String apiVersion) {
+			this.apiVersion = apiVersion;
+			return this;
+		}
+
+		/**
+		 * The deployment name you chose when you deployed the model.
+		 */
+		public Builder azureDeployment(String azureDeployment) {
+			this.azureDeployment = azureDeployment;
+			return this;
+		}
+
+		public Configuration build() {
+			return new Configuration(this);
+		}
 	}
 
-	@Value.Default
-	default Client client() {
-		return new Http2Client();
+	public String apiKey() {
+		return this.apiKey;
 	}
 
-	@Value.Default
-	default Retryer retryer() {
-		return new Retryer.Default(TimeUnit.SECONDS.toMillis(2),
-				TimeUnit.SECONDS.toMillis(2), 3);
+	public String organization() {
+		return this.organization;
 	}
 
-	@Value.Default
-	default Request.Options feignOptions() {
-		return new Request.Options(10, TimeUnit.SECONDS, 60, TimeUnit.SECONDS, true);
+	public String baseUrl() {
+		return this.baseUrl;
 	}
 
-	@Value.Default
-	default Logger logger() {
-		return new Slf4jLogger();
+	public Client client() {
+		return this.client;
 	}
 
-	@Value.Default
-	default ErrorDecoder errorDecoder() {
-		return new OpenAIErrorDecoder();
+	public Retryer retryer() {
+		return this.retryer;
 	}
 
-	@Nullable
-	RequestInterceptor additionalRequestInterceptor();
-
-	// Azure
-
-	@Value.Default
-	default Level logLevel() {
-		return Level.NONE;
+	public Request.Options feignOptions() {
+		return this.feignOptions;
 	}
 
-	/**
-	 * The name of your Azure OpenAI Resource.
-	 */
-	@Nullable
-	String azureEndpoint();
+	public Logger logger() {
+		return this.logger;
+	}
 
-	@Nullable
-	String apiVersion();
+	public ErrorDecoder errorDecoder() {
+		return this.errorDecoder;
+	}
 
-	/**
-	 * The deployment name you chose when you deployed the model.
-	 */
-	@Nullable
-	String azureDeployment();
+	public RequestInterceptor additionalRequestInterceptor() {
+		return this.additionalRequestInterceptor;
+	}
 
-	final class Builder extends ImmutableConfiguration.Builder {
+	public Level logLevel() {
+		return this.logLevel;
+	}
+
+	public String azureEndpoint() {
+		return this.azureEndpoint;
+	}
+
+	public String apiVersion() {
+		return this.apiVersion;
+	}
+
+	public String azureDeployment() {
+		return this.azureDeployment;
 	}
 }
